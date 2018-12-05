12a53b01b33944bb959fe38fe6ddc2afc2a237a0
cassiobotaro <cassiobotaro@gmail.com>
2018-12-05 20:18:25 -0200

    minimal structure

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # :joy: Flask Joke
 The Funniest Joke in the Flask World
+
+
+## Usage
+
+```python
+>>> import funniest
+>>> print(funniest.joke())
+```
+
+## Developing
+
+```bash
+$ pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flask_joke',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/cassiobotaro/flask_joke',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['flask_joke'],
+      zip_safe=False)
